2c670c0e44ffdafc5ee79550fd69f7257199208a
Jason Teplitz <jteplitz602@gmail.com>
2013-12-10 15:36:30 -0800

    added a bunch more examples and a favicon and a footer. Also cleaned up some mobile code

--- js/cards.js
+++ js/cards.js
@@ -16,5 +16,41 @@ var cards = [
     logo: "img/logos/linux.png",
     descp: "Linux is a free and open source operating system developed by Linus Torvalds.",
     url: "http://linux.org"
+  },
+  {
+    title: "WordPress",
+    logo: "img/logos/wordpress.png",
+    descp: "Wordpress is a free and open source blogging and content managing system. It powers over 60 million websites.",
+    url: "http://wordpress.org"
+  },
+  {
+    title: "Node.js",
+    logo: "img/logos/node.png",
+    descp: "Node.js is an open source Javascript server platform built on top of Google's V8 Runtime. It is used by companies such as Microsoft, Yahoo, and Paypal.",
+    url: "http://nodejs.org"
+  },
+  {
+    title: "JQuery",
+    logo: "img/logos/jquery.png",
+    descp: "JQuery is an open source Javascript library. Web developers use it to help them create powerful cross-browser webpages. We make use of it on this site.",
+    url: "http://jquery.com"
+  },
+  {
+    title: "Bootstrap",
+    logo: "img/logos/bootstrap.jpg",
+    descp: "Bootstrap is an open source CSS framework developed at Twitter. We use it to help style this site.",
+    url: "http://getbootstrap.com"
+  },
+  {
+    title: "Timeline JS",
+    logo: "img/logos/timelinejs.png",
+    descp: "Timeline JS is an open source timeline generator written in Javascript. It was developed by KnightLab at Northwestern University. We used it to make that super snazzy timeline on this site.",
+    url: "http://timeline.knightlab.com/"
+  },
+  {
+    title: "This Website",
+    logo: "img/logo.png",
+    descp: "This very website is open source. You can check out our source code at the github repo link below.",
+    url: "https://github.com/jteplitz602/Open-Source-History"
   }
 ];
